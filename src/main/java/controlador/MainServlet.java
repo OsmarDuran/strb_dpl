6936713fce425a7aa66d69e15fbb02ed1f59ddd5
Osmar <osmarhdezduran@gmail.com>
2025-01-31 15:54:48 -0600

    Configuración de servlet raíz

---
+++ src/main/java/controlador/MainServlet.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MainServlet", urlPatterns = {"/"})
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>¡Bienvenido a la aplicación de Rutas Barrido!</h1>");
+    }
+}
